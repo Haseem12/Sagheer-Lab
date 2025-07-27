ce8071dbd25d2a2a1d998107e696b94bf11ed1c4
Sagheer+ Lab <81861148+Haseem12@users.noreply.github.com>
2025-07-27 15:50:31 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
 import { About } from "./components/About";
 import { Pillars } from "./components/Pillars";
-import { ProjectShowcase } from "./components/ProjectShowcase";
+import { ProjectShowcase } from "../components/ProjectShowcase";
 import { Journal } from "./components/Journal";
 import { Opportunities } from "./components/Opportunities";
 import { Footer } from "./components/Footer";
