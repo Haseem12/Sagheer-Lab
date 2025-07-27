972813d2a10ef6c753aa48492a49f08ee57a3df5
Sagheer+ Lab <81861148+Haseem12@users.noreply.github.com>
2025-07-27 15:48:37 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
 import { About } from "./components/About";
-import { Pillars } from "./components/Pillars";
-import { ProjectShowcase } from "./components/ProjectShowcase";
-import { Journal } from "./components/Journal";
-import { Opportunities } from "./components/Opportunities";
-import { Footer } from "./components/Footer";
+import { Pillars } from "../components/Pillars";
+import { ProjectShowcase } from "../components/ProjectShowcase";
+import { Journal } from "../components/Journal";
+import { Opportunities } from "../components/Opportunities";
+import { Footer } from "../components/Footer";
 
 export default function App() {
   return (
